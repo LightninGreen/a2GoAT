2e68222df9fc66b3530d785d2d84e46ce1852548
LightninGreen <rdbaker@mta.ca>
2014-05-27 15:33:04 +0200

    GBakerCalibration.h now empty

--- inc/GBakerCalibration.h
+++ inc/GBakerCalibration.h
@@ -28,49 +28,10 @@ class	GBakerCalibration : public GoATTreeManager
 {
 private:
 
-	char 		cutfilename[256];
-	char 		cutname[256];	
-	
-	TFile* 		CutFile;
-	TCutG* 		Cut;
-   	TCutG* 		Cut_CB_proton; 
-   	TCutG* 		Cut_CB_pion; 
-   	TCutG*		Cut_CB_electron;
-   	TCutG* 		Cut_TAPS_proton; 
-   	TCutG* 		Cut_TAPS_pion; 
-   	TCutG*		Cut_TAPS_electron;
-	
-	Int_t 		ReconstructChargedParticles;
-	Double_t	charged_theta_min;
-	Double_t	charged_theta_max;
-	
-   	Int_t		Cut_CB_proton_active;   	
-   	Int_t		Cut_TAPS_proton_active;  
-   	Int_t 		Cut_proton_active;
-   	
-   	Int_t		Cut_CB_pion_active;
-   	Int_t		Cut_TAPS_pion_active;
-   	Int_t 		Cut_pion_active;
-   	
-   	Int_t		Cut_CB_electron_active; 
-   	Int_t		Cut_TAPS_electron_active; 
-   	Int_t 		Cut_electron_active;
-   	   	   	
-   	TCutG* 		Cut_proton;
-   	TCutG*		Cut_pion;
-   	TCutG*		Cut_electron;
-   	
-	Int_t		ReconstructMesons;
-	Double_t	meson_theta_min;
-	Double_t	meson_theta_max;
-		
-	Double_t	width_pi0;
-	Double_t	width_eta;
-	Double_t	width_etaP;
 
-	Int_t* 		Identified;
-    Int_t* 		Charge;
-	
+	Int_t* 	        Identified;
+	Int_t*	        Charge;	
+
 	Int_t 		nParticles;
 	Int_t 		nDaughterList;
    	Int_t 		i;
@@ -81,25 +42,8 @@ public:
 
 	GBakerCalibration();
 	~GBakerCalibration();
-
-	virtual void	Analyse() {;}
-	virtual void	Reconstruct();
-
-    
-	Bool_t	PostInit();
-	void	InitEvent();
-	void	CheckNeutrality();
-	void 	PhotonReconstruction();	
-	void 	ChargedReconstruction();
-	void 	MesonReconstruction();	
-	void	AddParticle(Int_t pdg_code, Int_t nindex, Int_t index_list[]);
-	void	AddParticle(Int_t pdg_code, Int_t i) 
-			{Int_t index_list[1]; index_list[0] = i; AddParticle(pdg_code, 1, index_list);}
-
-	TCutG*	OpenCutFile(Char_t* filename, Char_t* cutname);
-
-	// Make some variables available for sorting/printing
- 
+	
+	virtual void Reconstruct();
 
 };
 
